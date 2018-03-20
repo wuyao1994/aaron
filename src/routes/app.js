c34f679dbbb9d1b7064f1b35932893606f071f5c
wuyao1994 <wuyaoiot@163.com>
2018-03-21 00:03:58 +0800

    add sider page

--- src/routes/app.js
+++ src/routes/app.js
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types'
 import { config } from '../utils'
 import { withRouter } from 'dva/router'
 import { connect } from 'dva'
-import { Spin } from 'antd'
+import { Spin, Layout } from 'antd'
+import styles from './app.less'
+const { Header, Footer, Sider, Content } = Layout;
+
 const { openPages } = config
 const App = ({children, dispatch, app, loading, location}) => {
   const {
@@ -14,13 +17,24 @@ const App = ({children, dispatch, app, loading, location}) => {
   if (openPages && openPages.includes(pathname)) {
     return (
       <div>
-        <Spin spinning={loading.effects['app/query']} />
+        <Spin size="large" className={styles.spin} spinning={loading.effects['app/query']}/>
         {children}
       </div>
     )
   }
 
-  return (<div>{children}</div>)
+  return (
+    <div>
+      <Spin size="large" className={styles.spin} spinning={loading.effects['app/query']}/>
+      <Layout>
+        <Sider>Sider</Sider>
+        <Layout>
+          <Header>Header</Header>
+          <Content>{children}</Content>
+          <Footer>Footer</Footer>
+        </Layout>
+      </Layout>
+    </div>)
 }
 
 App.propTypes = {
