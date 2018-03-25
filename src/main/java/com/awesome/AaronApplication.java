bb2207a3cce966353a5c2e353aa69ff1790bceb4
wuyao1994 <wuyaoiot@163.com>
2018-03-26 00:21:02 +0800

    add spring boot

--- src/main/java/com/awesome/AaronApplication.java
+++ src/main/java/com/awesome/AaronApplication.java
@@ -3,9 +3,14 @@ package com.awesome;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RestController
 @SpringBootApplication
 public class AaronApplication {
 
+	@RequestMapping("/")
+	String index() {
+		return "Hello Spring Boot";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AaronApplication.class, args);
 	}
