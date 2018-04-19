ef959a78461485db5d8373fef367e3b31803e3e5
wuyao1994 <wuyaoiot@163.com>
2018-04-19 18:20:24 +0800

    spring boot dubbo

--- upms/upms-rpc-api/src/main/java/com/upms/rpc/api/SysAccountService.java
+++ upms/upms-rpc-api/src/main/java/com/upms/rpc/api/SysAccountService.java
@@ -1,4 +1,17 @@
 package com.upms.rpc.api;
 
+import com.upms.dao.model.SysAccount;
+
+import java.util.List;
+
+/**
+ * SysAccountService interface
+ */
 public interface SysAccountService {
+	/**
+     * select accounts
+	 * @param username
+	 * @return
+	 */
+	public List<SysAccount> selectAccountsByUsername(String username);
 }
--- upms/upms-rpc-service/src/main/java/com/upms/rpc/service/UpmsRpcServiceApplication.java
+++ upms/upms-rpc-service/src/main/java/com/upms/rpc/service/UpmsRpcServiceApplication.java
@@ -1,4 +1,13 @@
 package com.upms.rpc.service;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class UpmsRpcServiceApplication {
+	public static void main(String[] args) {
+
+		SpringApplication.run(UpmsRpcServiceApplication.class, args);
+
+	}
 }
