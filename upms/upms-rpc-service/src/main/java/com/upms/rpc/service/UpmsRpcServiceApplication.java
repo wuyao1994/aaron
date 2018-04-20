6134fcd33b3c59db0f13b7d51ff2a16cc078fc03
wuyao1994 <wuyaoiot@163.com>
2018-04-20 18:04:10 +0800

    spring boot dubbo

--- upms/upms-rpc-service/src/main/java/com/upms/rpc/service/UpmsRpcServiceApplication.java
+++ upms/upms-rpc-service/src/main/java/com/upms/rpc/service/UpmsRpcServiceApplication.java
@@ -1,13 +1,24 @@
 package com.upms.rpc.service;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-public class UpmsRpcServiceApplication {
+@EnableTransactionManagement
+@MapperScan("com.upms.dao.mapper")
+public class UpmsRpcServiceApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 
 		SpringApplication.run(UpmsRpcServiceApplication.class, args);
 
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(UpmsRpcServiceApplication.class);
 	}
 }
