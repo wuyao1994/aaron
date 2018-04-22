9b74d6af02ee68dec93aeef4804dfdacf30c288f
wuyao1994 <wuyaoiot@163.com>
2018-04-23 00:06:29 +0800

    add upms server

---
+++ upms/upms-server/src/main/java/com/upms/server/UpmsServerApplication.java
@@ -0,0 +1,24 @@
+package com.upms.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+public class UpmsServerApplication extends SpringBootServletInitializer {
+	public static void main(String[] args) {
+
+		SpringApplication.run(UpmsServerApplication.class, args);
+
+	}
+
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(UpmsServerApplication.class);
+	}
+}
